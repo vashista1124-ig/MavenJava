c1cf53eb59dc0304d129f1093927a63b3321d504
walter1124-pi <iamunknown1124@gmail.com>
2025-11-20 00:11:31 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+this is a maven java project
+checking webhook.
+
+checking for auto build trigger on commit
---
+++ src/main/java/in/kmit/demoE31/App.java
@@ -0,0 +1,11 @@
+package in.kmit.demoE31;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Welcome to Jenkins!");
+         System.out.println("we are tryig webhooks!");
+    }
+}
